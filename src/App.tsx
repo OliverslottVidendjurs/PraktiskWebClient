c02c08425adad12a983b79bf7e9fc2526c0f8464
oliverslott <oliverslottsony@gmail.com>
2020-01-15 10:17:01 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -23,11 +23,11 @@ import Friends from './components/friends/Friends';
 console.log(process.env.NODE_ENV);
 
 const uploadLink = createUploadLink({
-	uri: process.env.NODE_ENV === "development" ? "http://localhost:5000/graphql" : "http://167.172.110.163:5000/graphql",
+	uri: process.env.NODE_ENV === "production" ? "http://167.172.110.163:5000/graphql" : "http://localhost:5000/graphql",
 	credentials: "include"
 });
 export let test = new SubscriptionClient(
-	process.env.NODE_ENV === "development" ? "ws://localhost:5000/graphql" : "ws://localhost:5000/graphql",
+	process.env.NODE_ENV === "production" ? "ws://localhost:5000/graphql" : "ws://localhost:5000/graphql" ,
 	{
 		reconnect: true
 	}
